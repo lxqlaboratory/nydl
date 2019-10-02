5ef053bdfdc4251bc186b4125d4d0515a9ec73f5
xqli <xqli@sdu.edu.cn>
2019-10-02 20:26:41 +0800

    Update stuChooseModular.vue

--- src/views/modular/stuChooseModular.vue
+++ src/views/modular/stuChooseModular.vue
@@ -1,123 +1,119 @@
 <template>
   <div class="app-container" >
-    <div class="portlet-title">
-      <div class="caption">
-        <span>&nbsp;&nbsp;请同学们选择模块划分！点击“提交”即可。导师确认后系统提示“已确认”</span>
+    <template v-if="isConfirm === '1'" >
+      <div class="portlet-title">
+        <div class="caption">
+          <span>&nbsp;&nbsp;您选择的某块已经确认</span>
+        </div>
       </div>
-    </div>
-
-    <div style="max-width: 80%; text-align: center; margin: 10px auto;">
-      <table class="content" cellspacing="10" width="80%" align="center">
-        <tbody><tr>
-                 <td colspan="1">一志愿模块:</td>
-                 <td colspan="1" align="center">
-                   <el-input v-model="modularName1" size="mini" />
-                 </td>
-                 <td colspan="1" align="left">
-                   <template v-if="isConfirm === '0'" >
-                     <el-button size="mini" class="submitBtn" @click="deleteModular1">删除</el-button>
-                   </template>
-                 </td>
-                 <!--<td colspan="1" align="left">-->
-                 <!--状态-->
-                 <!--</td>-->
-               </tr>
-          <tr>
-            <td colspan="1" align="right">二志愿模块:</td>
-            <td colspan="1" align="center">
-              <el-input v-model="modularName2" size="mini" />
-            </td>
-            <td colspan="1" align="left">
-              <template v-if="isConfirm === '0'" >
-                <el-button size="mini" class="submitBtn" @click="deleteModular2">删除</el-button>
-              </template>
-            </td>
-          <!--<td colspan="1" align="left">-->
-            <!--状态-->
-          <!--</td>-->
-          </tr>
-          <tr>
-            <td colspan="1" align="right">三志愿模块:</td>
-            <td colspan="1" align="center">
-              <el-input v-model="modularName3" size="mini" />
-            </td>
-            <td colspan="1" align="left">
-              <template v-if="isConfirm === '0'" >
-                <el-button size="mini" class="submitBtn" @click="deleteModular3">删除</el-button>
-              </template>
-            </td>
-          <!--<td colspan="1" align="left">-->
-            <!--状态-->
-          <!--</td>-->
-          </tr>
-        </tbody></table>
-    </div>
-
-    <div align="center">
-      <template v-if="isConfirm === '0'" >
-        <el-button size="mini" class="submitBtn" @click="submitModular">提交</el-button>
-      </template>
-    </div>
-
-    <div class="tpl-portlet-components">
-      <div style="font-size:18px;line-height:18px;padding: 0px 50px;color: steelblue;">
-        <span>模块列表</span>
+    </template>
+    <template v-else >
+      <div class="portlet-title">
+        <div class="caption">
+          <span>&nbsp;&nbsp;请同学们选择模块划分！点击“提交”即可。导师确认后系统提示“已确认”</span>
+        </div>
       </div>
-    </div>
 
-    <el-table
-      :data="modularList"
-      border
-      style="width: 100%;"
-      width="800"
-      size="mini"
-      :header-cell-style="{background:'#eef1f6',color:'#606266',fontSize: '14px'}"
-    >
-      <el-table-column
-        label="序号"
-        fixed="left"
-        width="70"
-        align="center"
-        color="black"
+      <div style="max-width: 80%; text-align: center; margin: 10px auto;">
+        <table class="content" cellspacing="10" width="80%" align="center">
+          <tbody><tr>
+                  <td colspan="1">一志愿模块:</td>
+                  <td colspan="1" align="center">
+                    <el-input v-model="modularName1" size="mini" />
+                  </td>
+                  <td colspan="1" align="left">
+                    <template v-if="isConfirm === '0'" >
+                      <el-button size="mini" class="submitBtn" @click="deleteModular1">删除</el-button>
+                    </template>
+                  </td>
+                </tr>
+            <tr>
+              <td colspan="1" align="right">二志愿模块:</td>
+              <td colspan="1" align="center">
+                <el-input v-model="modularName2" size="mini" />
+              </td>
+              <td colspan="1" align="left">
+                <template v-if="isConfirm === '0'" >
+                  <el-button size="mini" class="submitBtn" @click="deleteModular2">删除</el-button>
+                </template>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="1" align="right">三志愿模块:</td>
+              <td colspan="1" align="center">
+                <el-input v-model="modularName3" size="mini" />
+              </td>
+              <td colspan="1" align="left">
+                <template v-if="isConfirm === '0'" >
+                  <el-button size="mini" class="submitBtn" @click="deleteModular3">删除</el-button>
+                </template>
+              </td>
+            </tr>
+          </tbody></table>
+      </div>
+
+      <div align="center">
+          <el-button size="mini" class="submitBtn" @click="submitModular">提交</el-button>
+      </div>
+
+      <div class="tpl-portlet-components">
+        <div style="font-size:18px;line-height:18px;padding: 0px 50px;color: steelblue;">
+          <span>模块列表</span>
+        </div>
+      </div>
+
+      <el-table
+        :data="modularList"
+        border
+        style="width: 100%;"
+        width="800"
+        size="mini"
+        :header-cell-style="{background:'#eef1f6',color:'#606266',fontSize: '14px'}"
       >
-        <template slot-scope="scope">
-          {{ scope.$index+1 }}
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="模块名称"
-        fixed="left"
-        align="center"
-        color="black"
-      >
-        <template slot-scope="scope">
-          {{ scope.row.modularName }}
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="模块描述"
-        fixed="left"
-        width="500"
-        align="center"
-        color="black"
-      >
-        <template slot-scope="scope">
-          {{ scope.row.modularDescription }}
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="操作"
-        fixed="left"
-        align="center"
-        color="black"
-      >
-        <template slot-scope="scope">
-          <template v-if="isConfirm === '0'" >
+        <el-table-column
+          label="序号"
+          fixed="left"
+          width="70"
+          align="center"
+          color="black"
+        >
+          <template slot-scope="scope">
+            {{ scope.$index+1 }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="模块名称"
+          fixed="left"
+          align="center"
+          color="black"
+        >
+          <template slot-scope="scope">
+            {{ scope.row.modularName }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="模块描述"
+          fixed="left"
+          width="500"
+          align="center"
+          color="black"
+        >
+          <template slot-scope="scope">
+            {{ scope.row.modularDescription }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          fixed="left"
+          align="center"
+          color="black"
+        >
+          <template slot-scope="scope">
             <el-button class="submitBtn" size="mini" @click="submit(scope.row.modularName,scope.row.modularId)">选择</el-button>
           </template>
-        </template>
-      </el-table-column>
-    </el-table>
+        </el-table-column>
+      </el-table>
+    </template>
   </div>
 </template>
 
